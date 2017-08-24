916c3ab1c74fd31f192a9e79e5ce9d4794327bdc
Yuya Sugihara <yuuyamix3@gmail.com>
2017-08-24 12:14:48 +0900

    Test.java

--- Test.java
+++ Test.java
@@ -2,7 +2,6 @@ public class Test{
   public static void main(String[] args){
     for(int i=0;i<3;i++){
       System.out.println("Hello World");
-      System.out.println("Hello javaWorld");
     }
   }
 }
